51904fb8130fb11d672de99204ba0e379e989b77
andynavy23 <andynavy23@gmail.com>
2021-09-28 16:31:37 +0800

    Add public to constructor function

--- contracts/GSN/Context.sol
+++ contracts/GSN/Context.sol
@@ -15,7 +15,7 @@ pragma solidity >=0.4.0;
 contract Context {
     // Empty internal constructor, to prevent people from mistakenly deploying
     // an instance of this contract, which should be used via inheritance.
-    constructor() {}
+    constructor() public {}
 
     function _msgSender() internal view returns (address) {
         return msg.sender;
