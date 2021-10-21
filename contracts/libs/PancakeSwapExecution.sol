565a1bcafbe9d6aa8b8ae442a92417e3b876880d
andynavy23 <andynavy23@gmail.com>
2021-10-21 15:51:10 +0800

    Modify PancakeSwapExecution contract functions type for gas optimizations

--- contracts/libs/PancakeSwapExecution.sol
+++ contracts/libs/PancakeSwapExecution.sol
@@ -20,20 +20,7 @@ library PancakeSwapExecution {
         address masterchef; // Address of PancakeSwap masterchef contract.
     }
     
-    // Info of each pool.
-    struct PoolInfo {
-        address lpToken;           // Address of LP token contract.
-        uint allocPoint;       // How many allocation points assigned to this pool. CAKEs to distribute per block.
-        uint lastRewardBlock;  // Last block number that CAKEs distribution occurs.
-        uint accCakePerShare; // Accumulated CAKEs per share, times 1e12. See below.
-    }
-    
-    function getBalanceBNB(address wallet_address, address BNB_address) public view returns (uint) {
-        
-        return IBEP20(BNB_address).balanceOf(wallet_address);
-    }
-    
-    function getLPBalance(address lp_token) public view returns (uint) {
+    function getLPBalance(address lp_token) external view returns (uint) {
         
         return IPancakePair(lp_token).balanceOf(address(this));
     }
@@ -41,47 +28,15 @@ library PancakeSwapExecution {
     /// @param lp_token_address PancakeSwap LPtoken address.
     /// @dev Gets the token0 and token1 addresses from LPtoken.
     /// @return token0, token1.
-    function getLPTokenAddresses(address lp_token_address) public view returns (address, address) {
+    function getLPTokenAddresses(address lp_token_address) external view returns (address, address) {
         
         return (IPancakePair(lp_token_address).token0(), IPancakePair(lp_token_address).token1());
-    }
-    
-    /// @param lp_token_address PancakeSwap LPtoken address.
-    /// @dev Gets the token0 and token1 symbol name from LPtoken.
-    /// @return token0, token1.
-    function getLPTokenSymbols(address lp_token_address) public view returns (string memory, string memory) {
-        (address token0, address token1) = getLPTokenAddresses(lp_token_address);
-        return (IBEP20(token0).symbol(), IBEP20(token1).symbol());
-    }
-    
-    /// @param self config of PancakeSwap.
-    /// @param pool_id Id of the PancakeSwap pool.
-    /// @dev Gets pool info from the masterchef contract and stores results in an array.
-    /// @return pooInfo.
-    function getPoolInfo(PancakeSwapConfig memory self, uint pool_id) public view returns (address, uint256, uint256, uint256) {
-        
-        return MasterChef(self.masterchef).poolInfo(pool_id);
-    }
-    
-    function getReserves(address lp_token_address) public view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) {
-        
-        return IPancakePair(lp_token_address).getReserves();
-    }
-
-    /// @param self config of PancakeSwap.
-    /// @param token_a_addr BEP20 token address.
-    /// @param token_b_addr BEP20 token address.
-    /// @dev Returns the LP token address for the token pairs.
-    /// @return pair address.
-    function getPair(PancakeSwapConfig memory self, address token_a_addr, address token_b_addr) public view returns (address) {
-        
-        return IPancakeFactory(self.factory).getPair(token_a_addr, token_b_addr);
     }
     
     /// @param lp_token_amnt The LP token amount.
     /// @param lp_token_addr address of the LP token.
     /// @dev Returns the amount of token0, token1s the specified number of LP token represents.
-    function getLPConstituients(uint lp_token_amnt, address lp_token_addr) public view returns (uint, uint) {
+    function getLPConstituients(uint lp_token_amnt, address lp_token_addr) external view returns (uint, uint) {
         (uint reserve0, uint reserve1, uint blockTimestampLast) = IPancakePair(lp_token_addr).getReserves();
         uint total_supply = IPancakePair(lp_token_addr).totalSupply();
         
@@ -91,17 +46,11 @@ library PancakeSwapExecution {
     }
     
     /// @param self config of PancakeSwap.
-    function getPendingStakedCake(PancakeSwapConfig memory self, uint pool_id) public view returns (uint) {
-        
-        return MasterChef(self.masterchef).pendingCake(pool_id, address(this));
-    }
-    
-    /// @param self config of PancakeSwap.
     /// @param token_addr address of the BEP20 token.
     /// @param token_amnt amount of token to add.
     /// @param eth_amnt amount of BNB to add.
     /// @dev Adds a pair of tokens into a liquidity pool.
-    function addLiquidityETH(PancakeSwapConfig memory self, address token_addr, address eth_addr, uint token_amnt, uint eth_amnt) public returns (uint) {
+    function addLiquidityETH(PancakeSwapConfig memory self, address token_addr, address eth_addr, uint token_amnt, uint eth_amnt) external returns (uint) {
         IBEP20(token_addr).approve(self.router, token_amnt);
         (uint reserves0, uint reserves1, uint blockTimestampLast) = IPancakePair(IPancakeFactory(self.factory).getPair(token_addr, eth_addr)).getReserves();
         
@@ -118,7 +67,7 @@ library PancakeSwapExecution {
     /// @param a_amnt amount of token a to add.
     /// @param b_amnt amount of token b to add.
     /// @dev Adds a pair of tokens into a liquidity pool.
-    function addLiquidity(PancakeSwapConfig memory self, address token_a_addr, address token_b_addr, uint a_amnt, uint b_amnt) public returns (uint){
+    function addLiquidity(PancakeSwapConfig memory self, address token_a_addr, address token_b_addr, uint a_amnt, uint b_amnt) external returns (uint){
         
         IBEP20(token_a_addr).approve(self.router, a_amnt);
         IBEP20(token_b_addr).approve(self.router, b_amnt);
@@ -140,7 +89,7 @@ library PancakeSwapExecution {
     /// @param a_amnt amount of token a to remove.
     /// @param b_amnt amount of token b to remove.
     /// @dev Removes a pair of tokens from a liquidity pool.
-    function removeLiquidity(PancakeSwapConfig memory self, address lp_contract_addr, address token_a_addr, address token_b_addr, uint liquidity, uint a_amnt, uint b_amnt) public {
+    function removeLiquidity(PancakeSwapConfig memory self, address lp_contract_addr, address token_a_addr, address token_b_addr, uint liquidity, uint a_amnt, uint b_amnt) external {
         
         IBEP20(lp_contract_addr).approve(self.router, liquidity);
         IPancakeRouter02(self.router).removeLiquidity(token_a_addr, token_b_addr, liquidity, a_amnt, b_amnt, address(this), block.timestamp);
@@ -153,14 +102,14 @@ library PancakeSwapExecution {
     /// @param a_amnt amount of token a to remove.
     /// @param b_amnt amount of BNB to remove.
     /// @dev Removes a pair of tokens from a liquidity pool.
-    function removeLiquidityETH(PancakeSwapConfig memory self, address lp_contract_addr, address token_addr, uint liquidity, uint a_amnt, uint b_amnt) public {
+    function removeLiquidityETH(PancakeSwapConfig memory self, address lp_contract_addr, address token_addr, uint liquidity, uint a_amnt, uint b_amnt) external {
         
         IBEP20(lp_contract_addr).approve(self.router, liquidity);
         IPancakeRouter02(self.router).removeLiquidityETH(token_addr, liquidity, a_amnt, b_amnt, address(this), block.timestamp);
     }
     
     /// @param self config of PancakeSwap.
-    function getAmountsOut(PancakeSwapConfig memory self, address token_a_address, address token_b_address, uint amountIn) public view returns (uint) {
+    function getAmountsOut(PancakeSwapConfig memory self, address token_a_address, address token_b_address, uint amountIn) external view returns (uint) {
         address pair = IPancakeFactory(self.factory).getPair(token_a_address, token_b_address);
         (uint reserve0, uint reserve1, uint blockTimestampLast) = IPancakePair(pair).getReserves();
         if (token_a_address == IPancakePair(pair).token0()) {
@@ -171,17 +120,10 @@ library PancakeSwapExecution {
         }
     }
     
-    /// @param lp_token_address address of the LP token.
-    /// @dev Gets the current price for a pair.
-    function getPairPrice(address lp_token_address) public view returns (uint) {
-        (uint reserve0, uint reserve1, uint blockTimestampLast) = IPancakePair(lp_token_address).getReserves();
-        return reserve0 + reserve1;
-    }
-    
     /// @param self config of PancakeSwap.
     /// @param pool_id Id of the PancakeSwap pool.
     /// @dev Gets the current number of LP tokens staked in the pool.
-    function getStakedLP(PancakeSwapConfig memory self, uint pool_id) public view returns (uint) {
+    function getStakedLP(PancakeSwapConfig memory self, uint pool_id) external view returns (uint) {
         (uint amount, uint rewardDebt) = MasterChef(self.masterchef).userInfo(pool_id, address(this));
         return amount;
     }
@@ -189,7 +131,7 @@ library PancakeSwapExecution {
     /// @param self config of PancakeSwap.
     /// @param pool_id Id of the PancakeSwap pool.
     /// @dev Gets the pending CAKE amount for a partictular pool_id.
-    function getPendingFarmRewards(PancakeSwapConfig memory self, uint pool_id) public view returns (uint) {
+    function getPendingFarmRewards(PancakeSwapConfig memory self, uint pool_id) external view returns (uint) {
         
         return MasterChef(self.masterchef).pendingCake(pool_id, address(this));
     }
@@ -198,99 +140,18 @@ library PancakeSwapExecution {
     /// @param pool_id Id of the PancakeSwap pool.
     /// @param unstake_amount amount of LP tokens to unstake.
     /// @dev Removes 'unstake_amount' of LP tokens from 'pool_id'.
-    function unstakeLP(PancakeSwapConfig memory self, uint pool_id, uint unstake_amount) public returns (bool) {
+    function unstakeLP(PancakeSwapConfig memory self, uint pool_id, uint unstake_amount) external returns (bool) {
         MasterChef(self.masterchef).withdraw(pool_id, unstake_amount);
         return true;
-    }
-    
-    /// @param self config of PancakeSwap.
-    /// @param token_address address of BEP20 token.
-    /// @param USDT_address address of USDT token.
-    /// @dev Returns the USD price for a particular BEP20 token.
-    function getTokenPriceUSD(PancakeSwapConfig memory self, address token_address, address USDT_address) public view returns (uint) {
-        uint token_decimals = IBEP20(token_address).decimals();
-        uint min_amountIn = SafeMath.mul(1, 10**token_decimals);
-        address pair = IPancakeFactory(self.factory).getPair(token_address, USDT_address);
-        (uint reserve0, uint reserve1, uint blockTimestampLast) = IPancakePair(pair).getReserves();
-        uint price = IPancakeRouter02(self.router).getAmountOut(min_amountIn, reserve0, reserve1);
-        return price;
     }
     
     /// @param self config of PancakeSwap.
     /// @param pool_id Id of the PancakeSwap pool.
     /// @param stake_amount amount of LP tokens to stake.
     /// @dev Gets pending reward for the user from the specific pool_id.
-    function stakeLP(PancakeSwapConfig memory self, uint pool_id, uint stake_amount) public returns (bool) {
+    function stakeLP(PancakeSwapConfig memory self, uint pool_id, uint stake_amount) external returns (bool) {
         MasterChef(self.masterchef).deposit(pool_id, stake_amount);
         return true;
-    }
-    
-    /// @param token_addr address of BEP20 token.
-    /// @param stake_contract_addr address of PancakeSwap masterchef.
-    /// @param amount amount of CAKE tokens to stake.
-    /// @dev Enables a syrup staking pool on PancakeSwap.
-    function enablePool(address token_addr, address stake_contract_addr, uint amount) public returns (bool) {
-        IBEP20(token_addr).approve(stake_contract_addr, amount);
-        return true;
-    }
-    
-    /// @param lp_token_address address of PancakeSwap LPtoken.
-    /// @dev Enables a syrup staking pool on PancakeSwap.
-    function enableFarm(address lp_token_address) public returns (bool) {
-        IBEP20(lp_token_address).approve(0x73feaa1eE314F8c655E354234017bE2193C9E24E, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
-        return true;
-    }
-    
-    /// @param self config of PancakeSwap.
-    /// @param pool_id Id of the PancakeSwap pool.
-    /// @dev Get the number of tokens staked into the pool.
-    function getStakedPoolTokens(PancakeSwapConfig memory self, uint pool_id) public view returns (uint) {
-        (uint amount, uint rewardDebt) = MasterChef(self.masterchef).userInfo(pool_id, address(this));
-        return amount;
-    }
-    
-    /// @param self config of PancakeSwap.
-    /// @param pool_id Id of the PancakeSwap pool.
-    /// @dev Gets pending reward for the syrup pool.
-    function getPendingPoolRewards(PancakeSwapConfig memory self, uint pool_id) public view returns (uint) {
-        
-        return MasterChef(self.masterchef).pendingCake(pool_id, address(this));
-    }
-    
-    /// @param self config of PancakeSwap.
-    /// @param pool_id Id of the PancakeSwap pool.
-    /// @param stake_amount amount of CAKE tokens to stake.
-    /// @dev Adds 'stake_amount' of coins into the syrup pools.
-    function stakePool(PancakeSwapConfig memory self, uint pool_id, uint stake_amount) public returns (bool) {
-        MasterChef(self.masterchef).deposit(pool_id, stake_amount);
-        return true;
-    }
-    
-    /// @param self config of PancakeSwap.
-    /// @param pool_id Id of the PancakeSwap pool.
-    /// @param unstake_amount amount of CAKE tokens to unstake.
-    /// @dev Removes 'unstake_amount' of coins into the syrup pools.
-    function unstakePool(PancakeSwapConfig memory self, uint pool_id, uint unstake_amount) public returns (bool) {
-        MasterChef(self.masterchef).withdraw(pool_id, unstake_amount);
-        return true;
-    }
-
-    function splitTokensEvenly(uint token_a_bal, uint token_b_bal, uint pair_price, uint price_decimals) public pure returns (uint, uint) {
-        uint temp = SafeMath.mul(1, 10**price_decimals);
-        uint a_amount_required = SafeMath.div(SafeMath.mul(token_b_bal, temp), pair_price);
-        uint b_amount_required = SafeMath.div(SafeMath.mul(token_a_bal, temp), pair_price);
-        if (token_a_bal > a_amount_required) {
-            return (a_amount_required, token_b_bal);
-        } else if (token_b_bal > b_amount_required) {
-            return (token_a_bal, b_amount_required);
-        } else {
-            return (0, 0);
-        }
-    }
-
-    function getPairDecimals(address pair_address) public pure returns (uint) {
-        
-        return IPancakePair(pair_address).decimals();
     }
     
 }
--- test/TestPancakeSwapExecution.sol
+++ test/TestPancakeSwapExecution.sol
@@ -7,19 +7,6 @@ import "../contracts/libs/PancakeSwapExecution.sol";
 
 contract TestPancakeSwapExecution {
 
-  function testGetBalanceBNB() public {
-    address FakeFakeIBEP20Address = DeployedAddresses.FakeIBEP20();
-
-    // Parms
-    address wallet_address = msg.sender;
-    address BNB_address = FakeFakeIBEP20Address;
-    // Testing
-    uint result = PancakeSwapExecution.getBalanceBNB(wallet_address, BNB_address);
-    uint expected = 10;
-
-    Assert.equal(result, expected, "It should get the value 10 of BNB balance.");
-  }
-
   function testGetLPBalance() public {
     address FakePancakePairAddress = DeployedAddresses.FakePancakePair();
 
@@ -47,78 +34,6 @@ contract TestPancakeSwapExecution {
     Assert.equal(result_2, expected_2, "It should get the address of token1.");
   }
 
-  function testGetLPTokenSymbols() public {
-    address FakePancakePairAddress = DeployedAddresses.FakePancakePair();
-
-    // Parms
-    address lp_token_address = FakePancakePairAddress;
-    // Testing
-    (string memory result_1, string memory result_2) = PancakeSwapExecution.getLPTokenSymbols(lp_token_address);
-    string memory expected_1 = "symbol";
-    string memory expected_2 = "symbol";
-
-    Assert.equal(result_1, expected_1, "It should get the string of LPtoken token0.");
-    Assert.equal(result_2, expected_2, "It should get the string of LPtoken token1.");
-  }
-
-  function testGetPoolInfo() public {
-    address FakeMasterChefAddress = DeployedAddresses.FakeMasterChef();
-
-    // Parms
-    uint pool_id = 10;
-    PancakeSwapExecution.PancakeSwapConfig memory pancakeSwapConfig = PancakeSwapExecution.PancakeSwapConfig({
-      router: address(0),
-      factory: address(0),
-      masterchef: FakeMasterChefAddress
-    });
-    // Testing
-    (address result_1, uint256 result_2, uint256 result_3, uint256 result_4) = PancakeSwapExecution.getPoolInfo(pancakeSwapConfig, pool_id);
-    address expected_1 = FakeMasterChefAddress;
-    uint256 expected_2 = 10;
-    uint256 expected_3 = 10;
-    uint256 expected_4 = 10;
-
-    Assert.equal(result_1, expected_1, "It should get the address of lpToken.");
-    Assert.equal(result_2, expected_2, "It should get the value 10 of pool_info allocPoint.");
-    Assert.equal(result_3, expected_3, "It should get the value 10 of pool_info lastRewardBlock.");
-    Assert.equal(result_4, expected_4, "It should get the value 10 of pool_info accCakePerShare.");
-  }
-
-  function testGetReserves() public {
-    address FakePancakePairAddress = DeployedAddresses.FakePancakePair();
-
-    // Parms
-    address lp_token = FakePancakePairAddress;
-    // Testing
-    (uint112 result_1, uint112 result_2, uint32 result_3) = PancakeSwapExecution.getReserves(lp_token);
-    uint112 expected_1 = 10;
-    uint112 expected_2 = 10;
-    uint32 expected_3 = 10;
-
-    Assert.equal(result_1, expected_1, "It should get the value 10 of reserve0.");
-    Assert.equal(result_2, expected_2, "It should get the value 10 of reserve1.");
-    Assert.equal(result_3, expected_3, "It should get the value 10 of blockTimestampLast.");
-  }
-
-  function testGetPair() public {
-    address FakePancakePairAddress = DeployedAddresses.FakePancakePair();
-    address FakePancakeFactoryAddress = DeployedAddresses.FakePancakeFactory();
-
-    // Parms
-    address token_a_addr = address(0);
-    address token_b_addr = address(0);
-    PancakeSwapExecution.PancakeSwapConfig memory pancakeSwapConfig = PancakeSwapExecution.PancakeSwapConfig({
-      router: address(0),
-      factory: FakePancakeFactoryAddress,
-      masterchef: address(0)
-    });
-    // Testing
-    address result = PancakeSwapExecution.getPair(pancakeSwapConfig, token_a_addr, token_b_addr);
-    address expected = FakePancakePairAddress;
-
-    Assert.equal(result, expected, "It should get the address of pair.");
-  }
-
   function testGetLPConstituients() public {
     address FakePancakePairAddress = DeployedAddresses.FakePancakePair();
 
@@ -132,23 +47,6 @@ contract TestPancakeSwapExecution {
 
     Assert.equal(result_1, expected_1, "It should get the value 10 of token0 amount.");
     Assert.equal(result_2, expected_2, "It should get the value 10 of token1 amount.");
-  }
-
-  function testGetPendingStakedCake() public {
-    address FakeMasterChefAddress = DeployedAddresses.FakeMasterChef();
-
-    // Parms
-    uint pool_id = 10;
-    PancakeSwapExecution.PancakeSwapConfig memory pancakeSwapConfig = PancakeSwapExecution.PancakeSwapConfig({
-      router: address(0),
-      factory: address(0),
-      masterchef: FakeMasterChefAddress
-    });
-    // Testing
-    uint result = PancakeSwapExecution.getPendingStakedCake(pancakeSwapConfig, pool_id);
-    uint expected = 10;
-
-    Assert.equal(result, expected, "It should get the value 10 of Pending Staked Cake.");
   }
 
   function testAddLiquidityETH() public {
@@ -257,18 +155,6 @@ contract TestPancakeSwapExecution {
     Assert.equal(result, expected, "It should get the value 10 of amountOut.");
   }
 
-  function testGetPairPrice() public {
-    address FakePancakePairAddress = DeployedAddresses.FakePancakePair();
-
-    // Parms
-    address lp_token_address = FakePancakePairAddress;
-    // Testing
-    uint result = PancakeSwapExecution.getPairPrice(lp_token_address);
-    uint expected = 20;
-
-    Assert.equal(result, expected, "It should get the value 20 of PairPrice.");
-  }
-
   function testGetStakedLP() public {
     address FakeMasterChefAddress = DeployedAddresses.FakeMasterChef();
 
@@ -321,26 +207,6 @@ contract TestPancakeSwapExecution {
     Assert.equal(result, expected, "It should get the bool true.");
   }
 
-  function testGetTokenPriceUSD() public {
-    address FakeFakeIBEP20Address = DeployedAddresses.FakeIBEP20();
-    address FakePancakeFactoryAddress = DeployedAddresses.FakePancakeFactory();
-    address FakePancakeRouterAddress = DeployedAddresses.FakePancakeRouter();
-
-    // Parms
-    address token_address = FakeFakeIBEP20Address;
-    address USDT_address = FakeFakeIBEP20Address;
-    PancakeSwapExecution.PancakeSwapConfig memory pancakeSwapConfig = PancakeSwapExecution.PancakeSwapConfig({
-      router: FakePancakeRouterAddress,
-      factory: FakePancakeFactoryAddress,
-      masterchef: address(0)
-    });
-    // Testing
-    uint result = PancakeSwapExecution.getTokenPriceUSD(pancakeSwapConfig, token_address, USDT_address);
-    uint expected = 10;
-
-    Assert.equal(result, expected, "It should get the bool true.");
-  }
-
   function testStakeLP() public {
     address FakeMasterChefAddress = DeployedAddresses.FakeMasterChef();
 
@@ -357,136 +223,6 @@ contract TestPancakeSwapExecution {
     bool expected = true;
 
     Assert.equal(result, expected, "It should get the bool true.");
-  }
-
-  function testEnablePool() public {
-    address FakeFakeIBEP20Address = DeployedAddresses.FakeIBEP20();
-
-    // Parms
-    address token_addr = FakeFakeIBEP20Address;
-    address stake_contract_addr = FakeFakeIBEP20Address;
-    uint amount = 10;
-    // Testing
-    bool result = PancakeSwapExecution.enablePool(token_addr, stake_contract_addr, amount);
-    bool expected = true;
-
-    Assert.equal(result, expected, "It should get the bool true.");
-  }
-
-  function testEnableFarm() public {
-    address FakeFakeIBEP20Address = DeployedAddresses.FakeIBEP20();
-
-    // Parms
-    address lp_token_address = FakeFakeIBEP20Address;
-    // Testing
-    bool result = PancakeSwapExecution.enableFarm(lp_token_address);
-    bool expected = true;
-
-    Assert.equal(result, expected, "It should get the bool true.");
-  }
-
-  function testGetStakedPoolTokens() public {
-    address FakeMasterChefAddress = DeployedAddresses.FakeMasterChef();
-
-    // Parms
-    uint pool_id = 10;
-    PancakeSwapExecution.PancakeSwapConfig memory pancakeSwapConfig = PancakeSwapExecution.PancakeSwapConfig({
-      router: address(0),
-      factory: address(0),
-      masterchef: FakeMasterChefAddress
-    });
-    // Testing
-    uint result = PancakeSwapExecution.getStakedPoolTokens(pancakeSwapConfig, pool_id);
-    uint expected = 10;
-
-    Assert.equal(result, expected, "It should get the value 10 of Pool LPtoken amount.");
-  }
-
-  function testGetPendingPoolRewards() public {
-    address FakeMasterChefAddress = DeployedAddresses.FakeMasterChef();
-
-    // Parms
-    uint pool_id = 10;
-    PancakeSwapExecution.PancakeSwapConfig memory pancakeSwapConfig = PancakeSwapExecution.PancakeSwapConfig({
-      router: address(0),
-      factory: address(0),
-      masterchef: FakeMasterChefAddress
-    });
-    // Testing
-    uint result = PancakeSwapExecution.getPendingPoolRewards(pancakeSwapConfig, pool_id);
-    uint expected = 10;
-
-    Assert.equal(result, expected, "It should get the value 10 of Pending Pool Rewards amount.");
-  }
-
-  function testStakePool() public {
-    address FakeMasterChefAddress = DeployedAddresses.FakeMasterChef();
-
-    // Parms
-    uint pool_id = 10;
-    uint stake_amount = 10;
-    PancakeSwapExecution.PancakeSwapConfig memory pancakeSwapConfig = PancakeSwapExecution.PancakeSwapConfig({
-      router: address(0),
-      factory: address(0),
-      masterchef: FakeMasterChefAddress
-    });
-    // Testing
-    bool result = PancakeSwapExecution.stakePool(pancakeSwapConfig, pool_id, stake_amount);
-    bool expected = true;
-
-    Assert.equal(result, expected, "It should get the bool true.");
-  }
-
-  function testUnstakePool() public {
-    address FakeMasterChefAddress = DeployedAddresses.FakeMasterChef();
-
-    // Parms
-    uint pool_id = 10;
-    uint unstake_amount = 10;
-    PancakeSwapExecution.PancakeSwapConfig memory pancakeSwapConfig = PancakeSwapExecution.PancakeSwapConfig({
-      router: address(0),
-      factory: address(0),
-      masterchef: FakeMasterChefAddress
-    });
-    // Testing
-    bool result = PancakeSwapExecution.unstakePool(pancakeSwapConfig, pool_id, unstake_amount);
-    bool expected = true;
-
-    Assert.equal(result, expected, "It should get the bool true.");
-  }
-
-  function testSplitTokensEvenly() public {
-    // Testing
-    uint result_1;
-    uint result_2;
-    uint expected_1;
-    uint expected_2;
-    (result_1, result_2) = PancakeSwapExecution.splitTokensEvenly(10, 10, 10, 10);
-    (expected_1, expected_2) = (0, 0);
-    Assert.equal(result_1, expected_1, "It should get the value 0 of result 1.");
-    Assert.equal(result_2, expected_2, "It should get the value 0 of result 2.");
-
-    (result_1, result_2) = PancakeSwapExecution.splitTokensEvenly(10**11, 10, 10, 10);
-    (expected_1, expected_2) = (10**10, 10);
-    Assert.equal(result_1, expected_1, "It should get the value 10 power of 10 of result 1.");
-    Assert.equal(result_2, expected_2, "It should get the value 10 of result 2.");
-
-    (result_1, result_2) = PancakeSwapExecution.splitTokensEvenly(10, 10**11, 10, 10);
-    (expected_1, expected_2) = (10, 10**10);
-    Assert.equal(result_1, expected_1, "It should get the value 10 of result 1.");
-    Assert.equal(result_2, expected_2, "It should get the value 10 power of 10 of result 2.");
-  }
-
-  function testGetPairDecimals() public {
-    address FakePancakePairAddress = DeployedAddresses.FakePancakePair();
-
-    // Parms
-    address pair_address = FakePancakePairAddress;
-    // Testing
-    uint result = PancakeSwapExecution.getPairDecimals(pair_address);
-    uint expected = 10;
-
-    Assert.equal(result, expected, "It should get the value 10 of Pair Decimals.");
   }
 
 }
