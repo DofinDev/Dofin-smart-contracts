f5b96e9b0cb2e3553cbcb8a8e10f2a01677dae9f
andynavy23 <andynavy23@gmail.com>
2021-10-22 19:47:35 +0800

    Modify setting

--- truffle-config.js
+++ truffle-config.js
@@ -15,7 +15,7 @@ module.exports = {
       settings: {
         optimizer: {
           enabled: true,
-          runs: 500   // Optimize for how many times you intend to run the code
+          runs: 2000   // Optimize for how many times you intend to run the code
         },
         evmVersion: "istanbul" // Default: "istanbul"
       },
@@ -40,15 +40,15 @@ module.exports = {
     BSCTestnet: {
       provider: () => new HDWalletProvider(env.BSCTestnet_mnemonic, 'https://data-seed-prebsc-1-s1.binance.org:8545'),
       network_id: 97,
-      confirmations: 10,
-      timeoutBlocks: 200,
+      confirmations: 6,
+      timeoutBlocks: 1000,
       skipDryRun: true
     },
     BSCMainnet: {
       provider: () => new HDWalletProvider(env.BSCMainnet_mnemonic, 'https://bsc-dataseed.binance.org/'),
       network_id: 56,
-      confirmations: 10,
-      timeoutBlocks: 200,
+      confirmations: 6,
+      timeoutBlocks: 1000,
       skipDryRun: true
     }
   },
