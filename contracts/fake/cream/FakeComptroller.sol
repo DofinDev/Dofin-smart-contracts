03f004488b6eb90b3cb74ae0610eacbd39a6ed8f
Andrew <andynavy23@gmail.com>
2021-11-02 22:38:48 +0800

    no message

--- contracts/fake/cream/FakeComptroller.sol
+++ contracts/fake/cream/FakeComptroller.sol
@@ -4,8 +4,10 @@ pragma solidity >=0.8;
 contract FakeComptroller {
 
   function enterMarkets(address[] calldata cTokens) public returns(uint[] memory) {
-
-    return 10;
+    uint[] memory a = new uint[] (2);
+    a[0] = 10;
+    a[1] = 10;
+    return a;
   }
 
   function exitMarket(address cToken) public returns(uint) {
